f2a239c3239e5593b09d7d39cf53ac8c9125448f
SharapaGorg <sharapov.savely@yandex.ru>
2021-05-02 23:01:35 +0300

    + video-preview + logIn modal window

---
+++ components/VideoPreview.vue
@@ -0,0 +1,56 @@
+<template>
+  <nuxt-link :to = "redirectPage">
+    <div>
+      <b-image
+        class='anime_image'
+        :src="url"
+        :webp-fallback="url"
+        ratio="0.5by0.5"
+        rounded
+      ></b-image>
+      <div class="box anime_title">
+        <span class='main_title'>&nbsp;{{ title }}</span>
+        <span class='sub_title'>&nbsp;{{ timeCode }}</span>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "VideoPreview",
+  props: ['url', 'title', 'timeCode', 'redirectPage']
+}
+</script>
+
+<style scoped>
+
+.anime_image {
+  width: 80px;
+  height: 80px;
+  z-index: 1;
+}
+
+.anime_title {
+  position: relative;
+  bottom: 75px;
+  left: 65px;
+  width: 220px;
+  height: 70px;
+  background: #2c2c2c;
+  z-index: 0;
+}
+
+.main_title {
+  position: absolute;
+  top: 10px;
+  color: #cdcdd4;
+}
+
+.sub_title {
+  position: relative;
+  top: 10px;
+  color: #9c9ca6;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,49 @@
 <template>
   <section class="section">
-    <div class = "box primary_back" style = "position: relative; z-index : 1">
+    <div class="box primary_back grid" style="position: relative; z-index : 1">
 
-      <search :list = anime_list placeholder="e.g. Тёмный дворецкий"></search>
-<br/>
-      <b-switch v-model="isSwitchedCustom"
-                true-value="Войти на сайт"
-                false-value="Шакалить">
-        {{ isSwitchedCustom ? 'Войти на сайт' : 'Шакалить' }}
-      </b-switch>
+      <search :list=anime_list placeholder="e.g. Тёмный дворецкий" class = "col-start-2"/>
 
-      <b-modal v-model = "isSwitchedCustom" v-if = "isSwitchedCustom !== 'Шакалить'">
+      <br/>
+      <b-button style = "float : right; bottom : 7px" type = "is-success">Найти</b-button>
+      <b-field>
+        <b-switch v-model="isSwitchedCustom"
+                  true-value="Войти на сайт"
+                  false-value="Шакалить">
+          {{ isSwitchedCustom ? 'Войти на сайт' : 'Шакалить' }}
+        </b-switch>
+      </b-field>
 
-        <div class = "box">
-          OK
-        </div>
+      <b-modal v-model='isSwitchedCustom' v-if="isSwitchedCustom !== 'Шакалить'" has-modal-card aria-role="dialog"
+               aria-modal :destroy-on-hide="false" trap-focus aria-label="Example Modal">
+
+        <center>
+          <div class="box secondary_back" style="width: 300px">
+
+            <b-field>
+              <b-input placeholder="Логин или email" v-model="loginEmail">
+
+              </b-input>
+            </b-field>
+
+            <b-field :type="{ 'is-warning' : secondFieldCheck }">
+              <b-input placeholder="Пароль" type="password" v-model="pwd">
+              </b-input>
+            </b-field>
+
+            <b-button :loading="this.isSwitched" @click="userLogIn" class="is-primary" style="width: 85%">Вход
+            </b-button>
+
+          </div>
+        </center>
 
       </b-modal>
 
     </div>
-    <div class = "box secondary_back" style = "position: relative; bottom : 35px; z-index : 0">
-      OK
+    <div class="box secondary_back" style="position: relative; bottom : 35px; z-index : 0"><br>
+      <VideoPreview url="https://gen.jut.su/uploads/animethumbs/anime_nanatsu-no-taizai.jpg" title="7 Смертных грехов"
+                    time-code="16 серия (4 сезон)"
+                    redirect-page="/"/>
     </div>
   </section>
 </template>
@@ -28,12 +51,12 @@
 <style>
 
 .primary_back {
-  background : #515b3b !important;
-  color : white
+  background: #515b3b !important;
+  color: white
 }
 
 .secondary_back {
-  background : #1f1f1f !important;
+  background: #1f1f1f !important;
 }
 
 </style>
@@ -41,22 +64,37 @@
 <script>
 
 import Search from '~/components/search'
+import VideoPreview from "@/components/VideoPreview";
 
 export default {
   name: 'HomePage',
   data() {
     return {
-      selected : '',
       isSwitchedCustom: "Шакалить",
-      isSwitched: 'ok',
-      anime_list : ['Моя геройская академия', 'Магическая битва', 'Атака Титанов', 'Тёмный дворецкий'].sort()
+      isSwitched: false,
+      loginEmail: "",
+      pwd: "",
+      anime_list: ['Моя геройская академия', 'Магическая битва', 'Атака Титанов', 'Тёмный дворецкий'].sort()
     }
   },
   components: {
-    Search
+    Search,
+    VideoPreview
   },
   methods: {
-
+    userLogIn() {
+      this.isSwitched = true;
+      // back end req
+      this.isSwitched = false;
+      // window.$nuxt.$router.push('/') NO
+      // check for req ans, if success :
+      this.isSwitchedCustom = "Шакалить"
+    }
+  },
+  computed: {
+    secondFieldCheck() {
+      return (this.pwd.length < 9 && this.pwd !== "")
+    }
   }
 }
 </script>
